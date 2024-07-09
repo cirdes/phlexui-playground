2a3c9c3eb0d74bd705777b3999dee57afe9e278d
Cirdes <cirdes@gmail.com>
2024-07-09 21:06:23 +0000

    react working

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,12 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
+    "./app/views/**/*.rb",
+    "app/views/**/*.html.erb",
     './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
   ],
   prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
